e14995e37d9c742ecbc09e9e41f1ea559cb1fa55
Kashika906 <psingh121080@gmail.com>
2026-05-07 17:57:28 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,13 @@
+string = input("Enter a word: ")
+char = input("Enter a character: ")
+
+i = 0
+count = 0
+
+while i < len(string):
+    if string[i] == char:
+        count = count + 1
+    
+    i = i + 1
+
+print("The character occurred", count, "times")
